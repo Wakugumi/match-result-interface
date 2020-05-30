b625e95fab9111d73865585a4a3fd1bb2aee2521
AnandaRisyad <anandarisyad@gmail.com>
2020-05-30 18:25:20 +0700

    third
    bruh like i'm the only one who knows the detail and the only contributor so no need to attach details information XD

--- esport-scoreboard/src/app/app.component.ts
+++ esport-scoreboard/src/app/app.component.ts
@@ -9,15 +9,10 @@ export class AppComponent {
   title = 'esport-scoreboard';
 
   scores = [
-    {team : "Team Liquid", kill : 12},
-    {team : "Team Nigma", kill : 11},
-    {team : "Team Secret", kill : 8},
-    {team : "FlyTotheMoon", kill : 7},
-    {team : "OG", kill : 5},
-    {team : "Team Liquid", kill : 12},
-    {team : "Team Nigma", kill : 11},
-    {team : "Team Secret", kill : 8},
-    {team : "FlyTotheMoon", kill : 7},
-    {team : "OG", kill : 5}
+    {team : "Team Liquid", kill : 12, death: 4},
+    {team : "Team Nigma", kill : 11, death: 2},
+    {team : "Team Secret", kill : 8, death: 8},
+    {team : "FlyTotheMoon", kill : 7, death: 5},
+    {team : "OG", kill : 5, death: 12}
   ]
 }
