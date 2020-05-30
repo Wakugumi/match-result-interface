90643212a3ab23e9883a392bff1c8dc7be2db5db
Ananda Risyad <anandarisyad@gmail.com>
2020-05-31 00:44:42 +0700

    fourth
    fourth commit, still working on the CSV parser

--- esport-scoreboard/src/app/app.component.ts
+++ esport-scoreboard/src/app/app.component.ts
@@ -1,18 +1,70 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as csv from 'csvtojson';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+
+  constructor() {}
+  
+  scores=[]
+
+  ngOnInit(): void {
+    
+  }
+
   title = 'esport-scoreboard';
 
-  scores = [
-    {team : "Team Liquid", kill : 12, death: 4},
-    {team : "Team Nigma", kill : 11, death: 2},
-    {team : "Team Secret", kill : 8, death: 8},
-    {team : "FlyTotheMoon", kill : 7, death: 5},
-    {team : "OG", kill : 5, death: 12}
-  ]
+  button() {
+    var x = this.readCsv()
+    this.scores.push(x)
+    console.log(x)
+    console.log(this.readCsv())
+  }
+
+  readCsv() {
+    var file = document.querySelector("#file-input").files[0];
+    var result;
+    // perform validation on file type & size if required
+
+    // read the file
+    var reader = new FileReader();
+
+    // file reading started
+    reader.addEventListener('loadstart', function() {
+        console.log('File reading started');
+    });
+
+    // file reading finished successfully
+    reader.addEventListener('load', function(e) {
+      // contents of file in variable     
+        var text = e.target.result;
+        csv().fromString(text.toString())
+        .then((jsonObj)=>{
+          result = jsonObj;
+        })
+    });
+
+    // file reading failed
+    reader.addEventListener('error', function() {
+        alert('Error : Failed to read file');
+    });
+
+    // file read progress 
+    reader.addEventListener('progress', function(e) {
+        if(e.lengthComputable == true) {
+          var percent_read = Math.floor((e.loaded/e.total)*100);
+          console.log(percent_read + '% read');
+        }
+    });
+
+    // read as text file
+    reader.readAsText(file);
+    console.log("result = "+result)
+    return(result)
+  }
+  
 }
--- esport-scoreboard/src/assets/csv.js
+++ esport-scoreboard/src/assets/csv.js
@@ -1,50 +1,15 @@
-//var csv is the CSV file with headers
-function csvJSON(csv){
+var http = require('http');
+var fs = require('fs');
+var csv = require('csvtojson');
 
-    var lines=csv.split("\n");
-  
-    var result = [];
-  
-    // NOTE: If your columns contain commas in their values, you'll need
-    // to deal with those before doing the next step 
-    // (you might convert them to &&& or something, then covert them back later)
-    // jsfiddle showing the issue https://jsfiddle.net/
-    var headers=lines[0].split(",");
-  
-    for(var i=1;i<lines.length;i++){
-  
-        var obj = {};
-        var currentline=lines[i].split(",");
-  
-        for(var j=0;j<headers.length;j++){
-            obj[headers[j]] = currentline[j];
-        }
-  
-        result.push(obj);
-  
-    }
-  
-    //return result; //JavaScript object
-    return JSON.stringify(result); //JSON
-}
-
-function readTextFile(file)
-{
-    var rawFile = new XMLHttpRequest();
-    rawFile.open("GET", file, false);
-    rawFile.onreadystatechange = function ()
-    {
-        if(rawFile.readyState === 4)
-        {
-            if(rawFile.status === 200 || rawFile.status == 0)
-            {
-                var allText = rawFile.responseText;
-                alert(allText);
-            }
-        }
-    }
-    rawFile.send(null);
-}
-
-var uvuvvewe = readTextFile("C:/Users/USER/Documents/Project/GitHub/match-result-interface/esport-scoreboard/src/assets/Book1.csv");
-console.log(csvJSON(uvuvvewe));
+http.createServer(function (req, res) {
+  fs.readFile('Book1.csv', function(err, data) {
+    res.writeHead(200, {'Content-Type': 'text/csv'});
+    console.log(data)
+    csv().fromString(data)
+        .then((jsonObj)=>{
+          res.write(jsonObj);
+        })
+    return res.end();
+  });
+}).listen(8080);
